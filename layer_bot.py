349f14900057d7574212e3fbb875514573a0cb73
twintersx <twintersx@gmail.com>
2021-10-02 09:11:42 -0700

    pre cleanup before sockets

--- layer_bot.py
+++ layer_bot.py
@@ -1,8 +1,13 @@
-import os, random, imagehash, datetime, zlib, time
+import os
+from socket import socket
+from random import choice
+from zlib import crc32
+from datetime import datetime
+from time import time
 from PIL import Image
+from imagehash import average_hash
 
-startTime = time.time()
-
+startTime = time()
 sizes = []
 crcList = []
 hashes = []
@@ -11,34 +16,10 @@ traits = os.listdir('Traits')
 
 def runTimeInfo(pointInTime):
     if pointInTime == 'start':
-        print(f"Bot started on: {datetime.datetime.now().replace(microsecond = 0)}")
+        print(f"Bot started on: {datetime.now().replace(microsecond = 0)}")
     elif pointInTime == 'end':
-        endTime = round(time.time() - startTime)
-        print(f"Bot ran for: {endTime} seconds, {endTime/60} minutes OR {endTime/2400} hour(s).")
-
-def hashNFT(filePathName):
-    with Image.open(filePathName) as img:
-        hash = str(imagehash.average_hash(img))
-    return hash
-
-def getTraitData():
-    for tIndex in range(0, len(traits)):
-        variationData = []
-        clonedSizes = []
-        variations = os.listdir(os.path.join('Traits', traits[tIndex]))
-        
-        for vIndex in range(0, len(variations)):
-            variationPath = os.path.join('Traits', traits[tIndex], variations[vIndex])
-            clonedSizes.append(os.path.getsize(variationPath))
-
-        for size in clonedSizes:
-            percentOfVariation = round(100*clonedSizes.count(hash) / len(variations))
-            sizeData = [size, percentOfVariation]
-
-            if sizeData not in variationData:
-                variationData.append(sizeData)
-        
-        traitsData.append(variationData)
+        endTime = round(time() - startTime)
+        print(f"Bot finished. Runtime: {endTime}s")
 
 def desiredNFTCounts():
     desiredNFTs = 1
@@ -47,54 +28,75 @@ def desiredNFTCounts():
     print(f"Now creating {desiredNFTs} unique NFT images...")
     return desiredNFTs
 
+def hashNFT(filePathName):
+    with Image.open(filePathName) as img:
+        hash = str(average_hash(img))
+    return hash
+
+def getTraitData():
+    for tIndex in range(0, len(traits)):
+        variationData = []
+        clonedHashes = []
+        variations = os.listdir(os.path.join('Traits', traits[tIndex]))
+        
+        for vIndex in range(0, len(variations)):
+            variationPath = os.path.join('Traits', traits[tIndex], variations[vIndex])
+            hash = hashNFT(variationPath)
+            clonedHashes.append(hash)
+
+        for hash in clonedHashes:
+            percentOfVariation = round(100*clonedHashes.count(hash) / len(variations))
+            data = [hash, percentOfVariation]
+
+            if data not in variationData:
+                variationData.append(data)
+    
+        traitsData.append(variationData)
+    
 def pickRandomTraits(): 
     randomTraits = []  
-    for tIndex, trait in enumerate(traits):
+    for trait in traits:
         variationDir = os.path.join('Traits', trait)
-        randomVariation = random.choice(os.listdir(variationDir))
+        randomVariation = choice(os.listdir(variationDir))
         variationPath = os.path.join(variationDir, randomVariation)
-
-        #size = os.path.getsize(variationPath)
 
         randomTraits.append(Image.open(variationPath))
 
     return randomTraits
 
-def saveTraitStackAsNFT(filePathName):
-    workingTraits = pickRandomTraits()
-    stackOfLayers = workingTraits[0]  
-    for img in range(0, len(workingTraits)-1):
-        traitToLayer = workingTraits[img + 1]
-        stackOfLayers.paste(traitToLayer, (0,0), traitToLayer.convert('RGBA'))
-    flattenedTraits = stackOfLayers
-    flattenedTraits.save(filePathName, 'PNG')
+def saveTraitStackAsNFT(workingTraits, filePathName): 
+    imageStack = Image.new('RGBA', (500, 500))
+    for img in range(0, len(workingTraits)):
+        traitToLayer = workingTraits[img]
+        imageStack.paste(traitToLayer, (0,0), traitToLayer.convert('RGBA'))
+    imageStack.save(filePathName, 'PNG')
 
 def cyclicRedundancyCheckOnNFT(filePathName):
     prev = 0
     for eachLine in open(filePathName, "rb"):
-        prev = zlib.crc32(eachLine, prev)
+        prev = crc32(eachLine, prev)
     return "%X"%(prev & 0xFFFFFFFF)
 
 def currentNFTs():
     return(len(os.listdir('NFTs')))
 
-def addNewSizeCRCandHash(filePathName):
-    size = os.path.getsize(filePathName)
+def addNewSizeCRCandHash(size, crcValue, filePathName):
     sizes.append(size)
-
-    crcValue = cyclicRedundancyCheckOnNFT(filePathName)
     crcList.append(crcValue)
 
     hash = hashNFT(filePathName)
     hashes.append(hash)
 
 def main():
+    runTimeInfo('start')
+    
+    getTraitData()
     desiredNFTs = desiredNFTCounts()
 
     i = 1
     while currentNFTs() < desiredNFTs:
         filePathName = f'NFTs\\Tin Woodman #{i}.PNG'
-        saveTraitStackAsNFT(filePathName)
+        saveTraitStackAsNFT(pickRandomTraits(), filePathName)
 
         size = os.path.getsize(filePathName)
         if (size in sizes):
@@ -104,13 +106,12 @@ def main():
                 if (hash in hashes):
                     os.remove(filePathName)
             else:
-                addNewSizeCRCandHash(filePathName)
+                addNewSizeCRCandHash(size, crcValue, filePathName)
                 i += 1
         else:
-            addNewSizeCRCandHash(filePathName)
+            addNewSizeCRCandHash(size, cyclicRedundancyCheckOnNFT(filePathName), filePathName)
             i += 1  
 
-runTimeInfo('start')
-getTraitData()
-main()
-runTimeInfo('end')
+    runTimeInfo('end')
+
+
