c3f31eb32fa76b8a665b9747c52bccdd3a6fa61f
twintersx <twintersx@gmail.com>
2021-10-02 17:33:57 -0700

    simplify to only send image bytes not hash

--- layer_bot.py
+++ layer_bot.py
@@ -22,17 +22,12 @@ def runTimeInfo(pointInTime):
         endTime = round(time() - startTime)
         print(f"Bot finished. Runtime: {endTime}s")
 
-def desiredNFTCounts():
-    desiredNFTs = 1
-    for trait in traitsData:
-        desiredNFTs = desiredNFTs * len(trait)
-    print(f"Now creating {desiredNFTs} unique NFT images...")
-    return desiredNFTs
-
-def hashNFT(filePathName):
-    with Image.open(filePathName) as img:
-        hash = str(average_hash(img))
-    return hash
+def getServerIP():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.connect(("8.8.8.8", 80))
+    ip = sock.getsockname()[0]
+    sock.close()
+    return(ip)
 
 def getTraitData():
     for tIndex in range(0, len(traits)):
@@ -53,25 +48,33 @@ def getTraitData():
                 variationData.append(data)
     
         traitsData.append(variationData)
-    
-def pickRandomTraits(): 
-    randomTraits = []  
-    for trait in traits:
-        variationDir = os.path.join('Traits', trait)
-        randomVariation = choice(os.listdir(variationDir))
-        variationPath = os.path.join(variationDir, randomVariation)
 
-        randomTraits.append(Image.open(variationPath))
+def desiredNFTCounts():
+    desiredNFTs = 1
+    for trait in traitsData:
+        desiredNFTs = desiredNFTs * len(trait)
+    print(f"Now creating {desiredNFTs} unique NFT images...")
+    return desiredNFTs
 
-    return randomTraits
+def initializeSocket(sock):
+    if getServerIP() == '192.168.1.5':
+        socketType = 'server'
+        sock.bind(('0.0.0.0', 1200))
+        sock.listen(10)
+        s, addr = sock.accept()
+    else:
+        socketType = 'client'
+        sock.connect(('192.168.1.5', 1200))
 
-def saveTraitStackAsNFT(workingTraits, filePathName): 
-    imageStack = Image.new('RGBA', (500, 500))
-    for img in range(0, len(workingTraits)):
-        traitToLayer = workingTraits[img]
-        imageStack.paste(traitToLayer, (0,0), traitToLayer.convert('RGBA'))
-    imageStack.save(filePathName, 'PNG')
-    return imageStack
+    return s, socketType
+
+def currentNFTs():
+    return(len(os.listdir('NFTs')))
+
+def hashNFT(filePathName):
+    with Image.open(filePathName) as img:
+        hash = str(average_hash(img))
+    return hash
 
 def cyclicRedundancyCheckOnNFT(filePathName):
     prev = 0
@@ -79,27 +82,22 @@ def cyclicRedundancyCheckOnNFT(filePathName):
         prev = crc32(eachLine, prev)
     return "%X"%(prev & 0xFFFFFFFF)
 
-def currentNFTs():
-    return(len(os.listdir('NFTs')))
+def saveTraitStackAsNFT(filePathName): 
+    randomTraits = []  
+    for trait in traits:
+        variationDir = os.path.join('Traits', trait)
+        randomVariation = choice(os.listdir(variationDir))
+        variationPath = os.path.join(variationDir, randomVariation)
+        randomTraits.append(Image.open(variationPath))
 
-def getServerIP():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.connect(("8.8.8.8", 80))
-    ip = sock.getsockname()[0]
-    sock.close()
-    return(ip)
+    imageStack = Image.new('RGBA', (500, 500))
+    for img in range(0, len(randomTraits)):
+        traitToLayer = randomTraits[img]
+        imageStack.paste(traitToLayer, (0,0), traitToLayer.convert('RGBA'))
+    imageStack.save(filePathName, 'PNG')
+    return imageStack
 
-def getIncomingHash(sock):
-    while True:
-        s, addr = sock.accept()
-        hash = s.recv(16).decode()
-        if not hash: 
-            print("None hash recved: ", 'None')
-            return None 
-        print("hash recved: ", hash)
-        return s, hash
-
-def saveIncomingHash(filePathName, s):
+def saveIncomingImage(filePathName, s):
     def recv_msg(s):
         raw_msglen = recvall(s, 4)
         if not raw_msglen:
@@ -117,48 +115,55 @@ def saveIncomingHash(filePathName, s):
         return data
 
     imageStack = Image.open(io.BytesIO(recv_msg(s)))
-    print("image byte object data recieved")
     imageStack.save(filePathName, 'PNG')
-    sizes.append(os.path.getsize(filePathName))
-    crcList.append(cyclicRedundancyCheckOnNFT(filePathName))
-    hashes.append(hashNFT(filePathName))
 
-def sendHash(sock, hash, imageStack):
+def sendImage(sock, imageStack):
     imageByteArr = io.BytesIO()
     imageStack.save(imageByteArr, format='PNG')
     imageByteArr = imageByteArr.getvalue()
     structToSend = struct.pack('>I', len(imageByteArr)) + imageByteArr
-    imgHash = [hash, structToSend]
-    imgHashListToSend.append(imgHash)
-
-    sock.sendall(bytes(imgHashListToSend))
-
-def initializeSocket(sock):
-    if getServerIP() == '192.168.1.5':
-        socketType = 'server'
-        sock.bind(('0.0.0.0', 1200))
-        sock.listen(10)
-    else:
-        socketType = 'client'
-        sock.connect(('192.168.1.5', 1200))
-
-    return socketType
+    imgHashListToSend.append(hash + structToSend)
+    with open('your_file.txt', 'w') as f:
+        for item in imgHashListToSend:
+            f.write("%s\n" % item)
+    fileToSend = open('your_file.txt', 'r')
+    content = fileToSend.read()
+    sock.send(content.encode())
 
 def main():
     desiredNFTs = desiredNFTCounts()
     sock = socket.socket()
-    socketType = initializeSocket(sock)
+    s, socketType = initializeSocket(sock)
 
     i = 1
     while currentNFTs() < desiredNFTs:
+        filePathName = f'NFTs\\Tin Woodman #{i}.PNG'
         if socketType == 'server':
-            s, hash = getIncomingHash(sock)
-            if hash not in hashes and hash != 'None':
-                saveIncomingHash(f'NFTs\\Tin Woodman #{i}.PNG', s)
+            saveIncomingImage(filePathName, s)
+            
+            size = os.path.getsize(filePathName)
+            if (size in sizes):
+                crcValue = cyclicRedundancyCheckOnNFT(filePathName)
+                if (crcValue in crcList):
+                    hash = hashNFT(filePathName)
+                    if (hash in hashes):
+                        os.remove(filePathName)
+
+                else:
+                    sizes.append(size)
+                    crcList.append(crcValue)
+                    hash = hashNFT(filePathName)
+                    hashes.append(hash)
+                    i += 1
+            else:
+                sizes.append(size)
+                crcList.append(cyclicRedundancyCheckOnNFT(filePathName))
+                hash = hashNFT(filePathName)
+                hashes.append(hash)
                 i += 1
 
         filePathName = f'NFTs\\Tin Woodman #{i}.PNG'
-        imageStack = saveTraitStackAsNFT(pickRandomTraits(), filePathName)
+        imageStack = saveTraitStackAsNFT(filePathName)
 
         size = os.path.getsize(filePathName)
         if (size in sizes):
@@ -174,7 +179,7 @@ def main():
                 hash = hashNFT(filePathName)
                 hashes.append(hash)
                 if socketType == 'client':
-                    sendHash(sock, hash, imageStack)
+                    sendImage(sock, hash, imageStack)
                 i += 1
         else:
             sizes.append(size)
@@ -182,7 +187,7 @@ def main():
             hash = hashNFT(filePathName)
             hashes.append(hash)
             if socketType == 'client':
-                sendHash(sock, hash, imageStack)
+                sendImage(sock, hash, imageStack)
             i += 1
 
 runTimeInfo('start')
